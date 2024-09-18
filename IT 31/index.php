09c2a0894031d4141179ce904d38ba74b9f08484
Alicio-1 <aliciodasilvacruz@gmail.com>
2024-09-18 08:10:24 -0300

    Add files via upload

---
+++ IT 31/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+    <link rel= "stylesheet" href= "estilo.css"
+</head>
+<body>
+    <h1> versinamento com GIT/GITHUB </h1>
+    <h2> Aula 11/09 </h2>
+    <p>Executando git commit </p>
+    <a> href="pagina1.php">Ir para pagina 1</a>
+</body>
+</html>
